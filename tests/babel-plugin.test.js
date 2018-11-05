16b26a8d1e9735247d3974912070f863ce1e9508
Cole Turner <turner.cole@gmail.com>
2018-11-04 17:59:06 -0800

    fix(tests): pom -> solidselectors

--- tests/babel-plugin.test.js
+++ tests/babel-plugin.test.js
@@ -1,43 +1,45 @@
 import pluginTester from 'babel-plugin-tester';
 import plugin from '../src/babel/plugin-7';
 
+const { name } = require('../package.json');
+
 pluginTester({
   plugin,
   snapshot: true,
   tests: [
     // Standard, full dead code elimination
     `
-        import { createSelector } from 'pom';
+        import { createSelector } from '${name}';
         const selector = createSelector({ prefix: 'cool '});
       `,
     `
-        import POM from 'pom';
+        import POM from '${name}';
         const selector = POM.createSelector({ prefix: 'cool '});
     `,
     `
-        const { createSelector } = require('pom');
+        const { createSelector } = require('${name}');
         const selector = createSelector({ prefix: 'cool '});
     `,
     `
-        const POM = require('pom');
+        const POM = require('${name}');
         const selector = POM.createSelector({ prefix: 'cool '});
     `,
 
     // Leftover imports
     `
-        import POM, { createSelector, somethingElse } from 'pom';
+        import POM, { createSelector, somethingElse } from '${name}';
         const selector = createSelector({ prefix: 'cool '});
         POM.keepsThis();
         POM.keepsThat();
         somethingElse('stays too');
     `,
     `
-        const { createSelector, somethingElse } = require('pom');
+        const { createSelector, somethingElse } = require('${name}');
         const selector = createSelector({ prefix: 'cool '});
         somethingElse('stays put');
     `,
     `
-        const { createSelector, somethingElse } = require('pom');
+        const { createSelector, somethingElse } = require('${name}');
         const selector = createSelector({ prefix: 'cool '});
         somethingElse('stays put');
         somethingElse('stays put');
